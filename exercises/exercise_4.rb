cd338b7384eefabdf20b26b7cdc69dd2a2735624
alex <you@example.com>
2019-05-14 22:00:14 +0000

    finished exercises 4 and 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,16 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+storeA = Store.create(id: 3, name: 'Montreal', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+storeC = Store.create(id: 4, name: 'Waterloo', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+storeE = Store.create(id: 5, name: 'Vaughan', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true);
+@mens_stores.each do |store| 
+    puts "#{store.name} has men's apparel"
+end
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+@womens_stores.each do |store| 
+    puts "#{store.name} has women's apparel"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = Store.average(:annual_revenue)
+largest_stores = Store.where("annual_revenue > 1000000")
+puts "this is the #{total_revenue} total revenue for all stores"
+puts "this is the #{average_revenue} average revenue for all stores"
+number_of_large_stores = largest_stores.size
+puts "there are #{number_of_large_stores} stores with north of 1m revenue"
