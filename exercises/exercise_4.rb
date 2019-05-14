e0e970ce579d155ed4115da076a56bab51bcf60f
alex <you@example.com>
2019-05-14 22:52:16 +0000

    finished exercise 6

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,9 +6,9 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-storeA = Store.create(id: 3, name: 'Montreal', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
-storeC = Store.create(id: 4, name: 'Waterloo', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
-storeE = Store.create(id: 5, name: 'Vaughan', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+@storeA = Store.create(id: 3, name: 'Montreal', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+@storeC = Store.create(id: 4, name: 'Waterloo', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+@storeE = Store.create(id: 5, name: 'Vaughan', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 @mens_stores = Store.where(mens_apparel: true);
 @mens_stores.each do |store| 
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,5 +1,5 @@
 require_relative '../setup'
-require_relative './exercise_1'
+require_relative './exercise_1' 
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
@@ -8,4 +8,7 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+
+
+@storeB.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@storeC.employees.create(first_name: "Alex", last_name: "Smith", hourly_rate: 75)
